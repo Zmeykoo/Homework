08af0a4317c9846b371c1ca53108457fbc7c9652
Vladyslav Skrypchuk <skrypaslav@gmail.com>
2020-04-06 13:33:15 +0300

    Take changes!!!

--- task1.py
+++ task1.py
@@ -16,7 +16,7 @@ Now is better than never.
 Although never is often better than right now.[n 1]
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea—let's do more of those!"""
+Namespaces are one honking great idea—let's do more of thoseses!"""
 
 #task 1
 print('\n'+a.upper())
